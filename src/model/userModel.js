249f879ce7ec8c1158e8826043d53038886d36a0
sagarsamediya <sagarsamediya17@.com>
2022-07-06 13:59:52 +0530

    sagar

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -2,36 +2,38 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
     title: {
-        type :string,
+         type :string,
+         require : true,
+         enum:[Mr, Mrs, Miss]
+      },
+      
+    name: {
+        type : string,
+        require : true
+      },
+
+    phone: {
+        type : string,
         require : true,
-        enum:[Mr, Mrs, Miss]
+        unique: true
     },
- name: {
-    type : string,
-     require : true
-    },
- phone: {
-    type : string,
-     require : true,
-     unique: true
-    },
+
     email: { 
         type: String,
         required: true,
         unique: true
       },
-    
-    
+           
     password:{
-            require: true,
-            type: String,
+        require: true,
+        type: String,
               
-    },
- address: {
-   street: { type : string},
-   city: {type : string},
-   pincode: {type : string}
- },
+      },
+    address: {
+        street: { type : string},
+        city: {type : string},
+       pincode: {type : string}
+     },
    
   
 },{ timestamps: true });
