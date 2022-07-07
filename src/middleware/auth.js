91c969ac95fae376dafd04bb0cda5071e5c36dfa
rakeshdeyol <rakeshdeyol868@gmail.com>
2022-07-07 13:23:06 +0530

    auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,24 +1,37 @@
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const booksModel = require('../controller/bookController')
 
 
-const authenticate = function (req, res, next) {
+const authentication = function (req, res, next) {
   try {
-    let token = req.headers["x-api-key"];
-    if (!token) token = req.headers["x-Api-key"];
-    if (!token)
-      {return res.status(400).send({ status: false, msg: "token must be present" })};
+    let token = req.headers["x-api-key"].toLowerCase();
+    
+    if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
 
     
-    if (token.length != 215){ return res.status(400).send({ status: false, msg: "token must be valid" })};
+    if (token.length != token) return res.status(400).send({ status: false, msg: "invalid token" });
     let decodedToken = jwt.verify(token, "project_3_Group-64");
     //console.log(decodedToken)
-    if (decodedToken.length ==0){
-      return res.status(404).send({ status: false, msg: "token is not valid" })};
+    if (!decodedToken) return res.status(404).send({ status: false, msg: "token is not valid" })
+    
+        
+      req.loggedInUser = decodedToken.userId   //  MAKING IT ACESSECIABLLE ANY WAHERE
+
     next();
     } catch (err) {
     return res.status(500).send({ msg: "Error", error: err.message });
   }
 };
 
-module.exports.authenticate=authenticate;
+const authorization = function(req,res,next){
+    try{
+    req.params.bookId != req.loggedInUser
+     return res.status(403).send({status:false,msg:'unauthorize user'})
+
+}catch (err) {
+    return res.status(500).send({ msg: "Error", error: err.message });
+  }
+}
+
+module.exports = { authentication,authorization }
