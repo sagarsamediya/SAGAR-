d3c1407b7aa93595faccb8538bcadf76cb27fc52
rakeshdeyol <rakeshdeyol868@gmail.com>
2022-07-07 11:24:32 +0530

    token add

--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://vishal221:QbG4QZXzT3SrfBAF@cluster0.jegkx.mongodb.net/group64Database",
-
-{useNewUrlParser: true})
+mongoose.connect("mongodb+srv://vishal221:QbG4QZXzT3SrfBAF@cluster0.jegkx.mongodb.net/group64Database",{
+    useNewUrlParser: true})
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
--- src/route/route.js
+++ src/route/route.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router =express.Router();
 const userController = require("../controller/userController")
+const login = require("../controller/login")
+
 
 
 
 
 router.post("/register",userController.createUser)
 
-// router.post("/login",)
+ router.post("/login",login.userLogin)
 
 // router.post("/books",)
 
