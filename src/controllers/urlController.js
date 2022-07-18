586d5e85a67fb81354f25fd651a3e03bffe13b89
sagarsamediya <sagarsamediya17@.com>
2022-07-18 15:51:04 +0530

    update

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,6 +1,7 @@
 const urlModel = require("../models/urlModel");
 const validUrl = require("valid-url");
 const shortid = require("shortid");
+const { isValid } = require("shortid");
 
 //----------------------------------------------------------------------------------------
 //                                1. API -  POST/url/shorten
@@ -9,18 +10,20 @@ const shortid = require("shortid");
 const createUrl = async function (req, res) {
   try {
     console.log("Create URL.");
+    const body = req.body;
+    if(Object.keys.value(length===0)) return res.status(400).send({status:false, msg: "body can not be empty"})
+   
+    const{longUrl} = body;
+    if(!isValid(longUrl))  return res.status(400).send({status:false, msg: "provide valid longUrl"})
+    // let { longUrl } = req.body ; 
+    // let shortUrl ;
+    // let urlCode ;
+    let  checkLongUrl = await urlModel.findOne({longUrl: longUrl})
+    if(checkLongUrl) return res.status(400).send({status: false , msg: "Already exit url"})
 
-    let { longUrl } = req.body ; 
+    let createUrl = await urlModel.create(body);
+    return res.status(201).send({status: true,message: "Successfully Generated Short URL.",data: "[]"});
 
-    let shortUrl ;
-    let urlCode ;
-
-
-    return res.status(201).send({
-      status: true,
-      message: "Successfully Generated Short URL.",
-      data: "[]",
-    });
   } catch (error) {
     return res.status(500).send({ status: false, message: error.message });
   }
@@ -33,7 +36,7 @@ const createUrl = async function (req, res) {
 const getUrl = async function (req, res) {
   try {
     console.log("GET URL.");
-
+     
     return res.status(200).send({
       status: true,
       message: "Successfully Fetched URL.",
