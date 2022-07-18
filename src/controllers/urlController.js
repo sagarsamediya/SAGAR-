4319ca295d4a6de220ea86e3d04911039dc3e23b
sagarsamediya <sagarsamediya17@.com>
2022-07-18 17:58:53 +0530

    update

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -8,26 +8,46 @@ const { isValid } = require("shortid");
 
 const createUrl = async function (req, res) {
   try {
-    // console.log("Create URL.");
+    console.log("Create URL.");
     const body = req.body;
        
        
-    if (Object.keys(body).length === 0) return res.status(400).send({ status: false, message: "Invalid Request Body: Body Empty." });
-    
+    if (Object.keys(body).length === 0) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Invalid Request Body: Body Empty." });
+    }
 
     let { longUrl } = body;
 
-    
-    if (typeof longUrl === "undefined" ||longUrl === null ) 
-      (typeof longUrl === "string" && longUrl.length === 0) 
-      return res.status(400).send({ status: false, message: "Please enter a valid <longUrl>." });
-    
-    // if(!/^(https:\/\/www\.|http:\/\/www\.|www\.)[a-zA-Z0-9\-_.$]+\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?/gm.test( longUrl)) ;
-    //   return res.status(400).send({ status: false, message: "<longURL> NOT a Valid URL Format." });
-    
+    if (
+      typeof longUrl === "undefined" ||
+      longUrl === null ||
+      (typeof longUrl === "string" && longUrl.length === 0)
+    ) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Please enter a valid <longUrl>." });
+    }
+
+    if (
+      !/^(https:\/\/www\.|http:\/\/www\.|www\.)[a-zA-Z0-9\-_.$]+\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?/gm.test(
+        longUrl
+      )
+    ) {
+      return res
+        .status(400)
+        .send({ status: false, message: "<longURL> NOT a Valid URL Format." });
+    }
+
     const longUrlUnique = await urlModel.findOne({ longUrl });
-    if (longUrlUnique) return res.status(400).send({ status: false,message: `<longURL>: <${longUrl}> Already Exists in Database.` });
-    
+    if (longUrlUnique) {
+      return res.status(400).send({
+        status: false,
+        message: `<longURL>: <${longUrl}> Already Exists in Database.`,
+      });
+    }
+
     let urlCode = shortid.generate().toLowerCase();
     let shortUrl = "localhost:3000/" + urlCode;
 
@@ -40,9 +60,13 @@ const createUrl = async function (req, res) {
       shortUrl: createData.shortUrl,
       urlCode: createData.urlCode,
     };
-    return res.status(201).send({status: true,message: "Successfully Generated Short URL.",data: result});
-  } catch (err) {
-    console.log(err)
+
+    return res.status(201).send({
+      status: true,
+      message: "Successfully Generated Short URL.",
+      data: result,
+    });
+  } catch (error) {
     return res.status(500).send({ status: false, message: error.message });
   }
 };
@@ -58,14 +82,14 @@ const getUrl = async function (req, res) {
     const urlCode = req.params.urlCode;
 
     const findUrl = await urlModel.findOne({ urlCode });
-     console.log(findUrl);
+
     if (!findUrl) {
       return res
         .status(404)
         .send({ status: false, message: "URL Not Found ." });
     }
 
-    return res.status(302).redirect(findUrl.longUrl);
+    return res.status(302).send(findUrl.longUrl);
   } catch (error) {
     return res.status(500).send({ status: false, message: error.message });
   }
